93f665b557709f1e6650cef263fa9c59d509a820
Murugamoorthy Parmilan <103124559+parmilan1998@users.noreply.github.com>
2025-01-26 23:42:04 +0530

    db connection changes

--- config/db.js
+++ config/db.js
@@ -1,11 +1,32 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB connected successfully!..");
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully!..");
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in the environment variables!");
+    }
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully!");
+    console.log("Mongo URI:", process.env.MONGO_URI);
   } catch (err) {
-    console.error(err.message);
+    console.error("MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
